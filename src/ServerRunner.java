3449971671e2e7689f414f9e8db8b464a97f8698
edwing santos <ehivan24@yahoo.com>
2016-04-30 15:00:52 -0400

    Streams

--- src/ServerRunner.java
+++ src/ServerRunner.java
@@ -1,8 +1,96 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerRunner {
 
 	public static void main(String[] args) {
+		List<Server> servers = new ArrayList<>();
+		Server one = new Server();
+		
+		one.setIp("192.09.091.12");
+		one.setManufacturer("Dell");
+		one.setName("web01");
+		one.setNumProcessors(9);
+		one.setRam(8);
+		one.setPurchaseDate(LocalDate.parse("2015-11-01", DateTimeFormatter.ISO_DATE));
+		
+		servers.add(one);
+		
+		
+		one = new Server();
+		one.setIp("192.09.091.121");
+		one.setManufacturer("HP");
+		one.setName("web02");
+		one.setNumProcessors(8);
+		one.setRam(7);
+		one.setPurchaseDate(LocalDate.parse("2010-12-01", DateTimeFormatter.ISO_DATE));
+		
+		servers.add(one);
+		
+		one = new Server();
+		one.setIp("192.09.091.125");
+		one.setManufacturer("HP");
+		one.setName("web24");
+		one.setNumProcessors(12);
+		one.setRam(17);
+		one.setPurchaseDate(LocalDate.parse("2010-12-01", DateTimeFormatter.ISO_DATE));
+		
+		servers.add(one);
+		
+		
+		one = new Server();
+		one.setIp("190.12.91.12");
+		one.setManufacturer("Dell-1");
+		one.setName("web09");
+		one.setNumProcessors(12);
+		one.setRam(9);
+		one.setPurchaseDate(LocalDate.parse("2012-01-01", DateTimeFormatter.ISO_DATE));
+
+		servers.add(one);
+		
+		
+		one = new Server();
+		one.setIp("192.09.10.12");
+		one.setManufacturer("Ubuntu");
+		one.setName("web12");
+		one.setNumProcessors(19);
+		one.setRam(18);
+		one.setPurchaseDate(LocalDate.parse("2015-01-01", DateTimeFormatter.ISO_DATE));
+		
+		servers.add(one);
+		
+		one = new Server();
+		one.setIp("190.019.09.125");
+		one.setManufacturer("Apache");
+		one.setName("web01");
+		one.setNumProcessors(9);
+		one.setRam(8);
+		one.setPurchaseDate(LocalDate.parse("2013-11-11", DateTimeFormatter.ISO_DATE));
+		
+		servers.add(one);
+		
+		
+		
+		String manufacturer = "HP";
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("Here are the names of the servers made by " + manufacturer + " currently in inventory:");
+
+		servers
+			.stream()
+			.filter(s -> s.getManufacturer().equalsIgnoreCase(manufacturer))
+			.forEach(s -> System.out.println(s.getName() +" <==> "+ s.getIp() ));
+		System.out.println("");
+		
+		
+		System.out.println("Prints out all the servers name by Name ");
+		servers
+			.stream()
+			.forEach(server -> System.out.println(server.getName()));
+		
+		
+		
 		
 	}
-
 }
